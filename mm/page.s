827e6eb9a9b096e0d406319f980bae4dfe27b2f3
MRNIU <a6813140@hotmail.com>
2018-06-29 00:24:47 +0800

    页异常中断处理

--- mm/page.s
+++ mm/page.s
@@ -1,13 +1,4 @@
-/*
- *  linux/mm/page.s
- *
- *  (C) 1991  Linus Torvalds
- */
-
-/*
- * page.s contains the low-level page-exception code.
- * the real work is done in mm.c
- */
+//page.s contains the low-level page-exception code.the real work is done in mm.c
 
 .globl page_fault       # 声明为全局变量。将在traps.c中用于设置页异常描述符。
 
