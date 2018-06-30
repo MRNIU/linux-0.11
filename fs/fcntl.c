71bea60d35a698e094ec89988055851691eed436
MRNIU <a6813140@hotmail.com>
2018-06-30 19:16:50 +0800

    Update:

--- fs/fcntl.c
+++ fs/fcntl.c
@@ -1,5 +1,5 @@
 #include <string.h>		// 字符串头文件。主要定义了一些有关字符串操作的嵌入函数。
-#include <errno.h>		// 错误号头文件。包含系统中各种出错号。(Linus 从minix 中引进的)。
+#include <errno.h>		// 错误号头文件。包含系统中各种出错号。(Linus 从 minix 中引进的)。
 #include <linux/sched.h>	// 调度程序头文件，定义了任务结构 task_struct、初始任务 0 的数据
 #include <linux/kernel.h>	// 内核头文件。含有一些内核常用函数的原形定义。
 #include <asm/segment.h>	// 段操作头文件。定义了有关段寄存器操作的嵌入式汇编函数。
